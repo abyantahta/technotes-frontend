1ed26d6db17d178fc2e04d1e40d05161134689ca
Abyantahta <abyantahta2002@gmail.com>
2024-09-08 17:15:26 +0700

    feat : JWT auth is done

---
+++ src/hooks/usePersist.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+
+const usePersist = () => {
+    const [persist,setPersist] = useState(JSON.parse(localStorage.getItem("persist")||false)) //persist = tetap/terus ada
+
+    useEffect(()=>{
+        localStorage.setItem("persist",JSON.stringify(persist))
+    },[persist])
+  return [persist,setPersist]
+}
+
+export default usePersist
