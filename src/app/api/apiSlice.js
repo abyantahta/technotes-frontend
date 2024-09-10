99d72a318b298e8cd8002c5e722e7a6a1114deec
Abyantahta <abyantahta2002@gmail.com>
2024-09-10 23:36:38 +0700

    feat - beres deh

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://main--api-technotes.netlify.app/',
+    baseUrl: 'https://api-technotes.netlify.app/',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
