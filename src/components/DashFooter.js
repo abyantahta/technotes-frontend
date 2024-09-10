cb8f963512efc8d07963c21665e75484406ac340
Abyantahta <abyantahta2002@gmail.com>
2024-09-10 22:59:22 +0700

    feat- update browserList

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate, useLocation } from 'react-router-dom'
